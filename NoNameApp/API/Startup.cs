901d29859899fcd1f4a692df8fc689a0d61328b7
Сергей Дмитрик <SerhiyDmytryk@gmail.com>
2019-12-01 22:09:14 +0200

    update connection strings

--- NoNameApp/API/Startup.cs
+++ NoNameApp/API/Startup.cs
@@ -3,12 +3,19 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Persistence;
 
 namespace API {
     public class Startup {
+
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services) {
-            var connectionString = "server=127.0.0.1;user id=root;password=98986476Serega;port=3306;database=noNameApp;";
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<NoNameContext>(options => options.UseMySql(connectionString));
 
             services.AddMvc(o => o.EnableEndpointRouting = false);
@@ -18,11 +25,11 @@ namespace API {
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions {
-                    HotModuleReplacement = true
-                });
             }
+
+            app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions {
+                HotModuleReplacement = true
+            });
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
