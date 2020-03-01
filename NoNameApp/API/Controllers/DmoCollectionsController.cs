a6a3fd0d6180219757f711010b45e13eb7b94fb8
Сергей Дмитрик <SerhiyDmytryk@gmail.com>
2020-03-01 21:55:30 +0200

    change dmo collections loaders

--- NoNameApp/API/Controllers/DmoCollectionsController.cs
+++ NoNameApp/API/Controllers/DmoCollectionsController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using API.DTO.DmoCollections;
 using API.Helpers;
