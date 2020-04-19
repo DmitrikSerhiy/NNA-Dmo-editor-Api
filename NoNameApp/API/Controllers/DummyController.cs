9bb67207a1e9503b14c59afd57c71ed947692042
Сергей Дмитрик <SerhiyDmytryk@gmail.com>
2020-04-19 20:45:47 +0300

    added dummy

---
+++ NoNameApp/API/Controllers/DummyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers {
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    [ApiController]
+    public class DummyController : ControllerBase {
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult<string> Hello() {
+            return Ok("Hello from dummy");
+        }
+    }
+}
