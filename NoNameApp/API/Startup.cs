0faa13c9452937c0629d109bdcf033d350c8f875
Сергей Дмитрик <SerhiyDmytryk@gmail.com>
2020-04-19 18:49:39 +0300

    update migration sequence added qa environment

--- NoNameApp/API/Startup.cs
+++ NoNameApp/API/Startup.cs
@@ -102,7 +102,7 @@ namespace API {
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-            } else {
+            } else if(env.IsEnvironment("QA")) {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
