1cfdc36a1e24f0d21700c43cb1f0b38ed5a8f1d9
Serhii Dmytryk <dr.voo1646@gmail.com>
2022-09-10 18:28:55 +0300

    add migration

--- src/NNA.Persistence/Migrations/20220910150225_UpdateAfterNet6Migration.Designer.cs
+++ src/NNA.Persistence/Migrations/20220910150225_UpdateAfterNet6Migration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NNA.Persistence;
 
@@ -11,9 +12,10 @@ using NNA.Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(NnaContext))]
-    partial class NnaContextModelSnapshot : ModelSnapshot
+    [Migration("20220910150225_UpdateAfterNet6Migration")]
+    partial class UpdateAfterNet6Migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/NNA.Persistence/MigrationsHelper.cs
+++ src/NNA.Persistence/MigrationsHelper.cs
@@ -4,11 +4,12 @@ using Microsoft.EntityFrameworkCore.Design;
 namespace NNA.Persistence;
 
 public sealed class MigrationsHelper: IDesignTimeDbContextFactory<NnaContext> {
-    
+
+    private static string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=nna-local;"; // change for  prod db manually
     public NnaContext CreateDbContext(string[] args) {
         
         var optionsBuilder = new DbContextOptionsBuilder<NnaContext>();
-        optionsBuilder.UseSqlServer(args[0], migrationOptions => {
+        optionsBuilder.UseSqlServer(_connectionString, migrationOptions => {
             migrationOptions.MigrationsAssembly("NNA.Persistence");
         });
 
