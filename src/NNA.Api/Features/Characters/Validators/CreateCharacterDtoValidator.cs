1cac48600d841652c8742fe6db661076bac634ad
Serhii Dmytryk <dr.voo1646@gmail.com>
2022-11-08 17:35:39 +0200

    add max length validation to characters

--- src/NNA.Api/Features/Characters/Validators/CreateCharacterDtoValidator.cs
+++ src/NNA.Api/Features/Characters/Validators/CreateCharacterDtoValidator.cs
@@ -10,7 +10,12 @@ public sealed class CreateCharacterDtoValidator: AbstractValidator<CreateCharact
             .WithMessage("Dmo id is missing");
         RuleFor(cha => cha.Name)
             .NotEmpty()
-            .WithMessage("Character's name is missing");
+            .WithMessage("Character's name is missing")
+            .MaximumLength(60)
+            .WithMessage("Maximum character's name length exceeded");
+        RuleFor(cha => cha.Aliases)
+            .MaximumLength(100)
+            .WithMessage("Maximum character's aliases length exceeded");
         RuleFor(cha => cha.Color)
             .NotEmpty()
             .WithMessage("Color is missing");
--- src/NNA.Api/Features/Characters/Validators/UpdateCharacterDtoValidator.cs
+++ src/NNA.Api/Features/Characters/Validators/UpdateCharacterDtoValidator.cs
@@ -13,7 +13,12 @@ public sealed class UpdateCharacterDtoValidator : AbstractValidator<UpdateCharac
             .WithMessage("Character's id is missing");
         RuleFor(cha => cha.Name)
             .NotEmpty()
-            .WithMessage("Character's name is missing");
+            .WithMessage("Character's name is missing")
+            .MaximumLength(60)
+            .WithMessage("Maximum character's name length exceeded");
+        RuleFor(cha => cha.Aliases)
+            .MaximumLength(100)
+            .WithMessage("Maximum character's aliases length exceeded");
         RuleFor(cha => cha.Color)
             .NotEmpty()
             .WithMessage("Color is missing");
