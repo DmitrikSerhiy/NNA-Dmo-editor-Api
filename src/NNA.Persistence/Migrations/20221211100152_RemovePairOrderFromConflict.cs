ee88139a9f74d128517764f6293bd61caae5230b
Serhii Dmytryk <dr.voo1646@gmail.com>
2022-12-11 12:11:41 +0200

    add migration files

---
+++ src/NNA.Persistence/Migrations/20221211100152_RemovePairOrderFromConflict.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class RemovePairOrderFromConflict : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PairOrder",
+                table: "NnaMovieCharacterConflicts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PairOrder",
+                table: "NnaMovieCharacterConflicts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
