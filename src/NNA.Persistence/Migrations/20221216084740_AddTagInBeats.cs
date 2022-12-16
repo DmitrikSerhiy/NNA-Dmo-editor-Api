ac7942840e8b10d78cf6515d672cde36c9f71795
Serhii Dmytryk <dr.voo1646@gmail.com>
2022-12-16 11:54:05 +0200

    add migration files

---
+++ src/NNA.Persistence/Migrations/20221216084740_AddTagInBeats.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class AddTagInBeats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TagInBeats",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TempId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BeatId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TagId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateOfCreation = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagInBeats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TagInBeats_Beats_BeatId",
+                        column: x => x.BeatId,
+                        principalTable: "Beats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TagInBeats_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagInBeats_BeatId",
+                table: "TagInBeats",
+                column: "BeatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagInBeats_TagId",
+                table: "TagInBeats",
+                column: "TagId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TagInBeats");
+        }
+    }
+}
